4a4624eb250dc753505b5110fdc6745f4331e72c
Vito Mengers <v.mengers@campus.tu-berlin.de>
2018-06-08 22:10:40 +0200

    updated delegation tests for current state

---
+++ src/delegation_tests/delegation_manager_test.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python2
+
+import unittest
+import rospy
+import rostest
+
+from delegation_components.delegation_manager import DelegationManager
+from behaviour_components.managers import Manager
+from delegation_tests.basic_sceanrio import BasicCookingRobot
+
+PKG = "task_decomposition_module"
+
+
+class DelegationManagerTest1(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(DelegationManagerTest1, self).__init__(*args, **kwargs)
+        rospy.init_node("TestNode1")
+
+    def test_basic_auction_with_multiple_bidders(self):
+
+        rospy.loginfo("Starting Basic Test of auctions")
+
+        delegation_manager = DelegationManager(instance_name="Test1", max_tasks=1)
+
+        prefix = "test_manager"
+
+        manager = Manager(prefix=prefix)
+
+        scenarioRobot = BasicCookingRobot(planner_prefix=prefix)
+
+        interface = manager.get_delegation_interface()
+
+        interface.register(delegation_manager=delegation_manager)
+
+        for i in range(3):
+            manager.step()
+            rospy.sleep(1)
+
+        test_condition = scenarioRobot.get_test_cond()
+
+        interface.delegate(name="test_goal", conditions=[test_condition], satisfaction_threshold=1.0)
+
+        for i in range(7):
+            manager.step()
+            rospy.sleep(1)
+
+        self.assertEqual(delegation_manager.get_delegation(auction_id=1).get_contractor(), "PassiveDelegationManager")
+
+
+
+if __name__ == '__main__':
+    rostest.rosrun(PKG, 'DelegationManagerTest', DelegationManagerTest1)
+
+
---
+++ src/delegation_tests/passive_node.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python2
+
+import rospy
+
+from delegation_components.delegation_manager import DelegationManager
+from behaviour_components.managers import Manager
+from delegation_tests.basic_sceanrio import BasicCookingRobot
+from delegation_components.cost_computing import AbstractCostEvaluator
+
+
+class AlwaysWinningCostEvaluator(AbstractCostEvaluator):
+
+    def __init__(self):
+        super(AlwaysWinningCostEvaluator, self).__init__()
+
+    def compute_cost_and_possibility(self, goal_representation):
+        self.__last_cost, self.__last_possibility = 0, True
+        return 0, True
+
+
+if __name__ == '__main__':
+
+    rospy.init_node(name="PassiveNode")
+
+    rospy.loginfo("Starting a passive node with the basic scenario")
+
+    delegation_manager = DelegationManager(instance_name="PassiveDelegationManager", max_tasks=10)
+
+    prefix = "PassiveManager"
+
+    manager = Manager(prefix=prefix)
+
+    scenarioRobot = BasicCookingRobot(planner_prefix=prefix)
+
+    interface = manager.get_delegation_interface()
+
+    interface.register(delegation_manager=delegation_manager, add_own_cost_evaluator=False)
+
+    evaluator = AlwaysWinningCostEvaluator()
+
+    delegation_manager.set_cost_function_evaluator(cost_function_evaluator=evaluator, manager_name=prefix)
+
+    while not rospy.is_shutdown():
+        try:
+            manager.step()
+        except Exception as e:
+            rospy.logwarn("Exception in PassiveNode step: " + e.message)
+        rospy.sleep(1)
+
+
+
